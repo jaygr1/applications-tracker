64671c16b864f3917ecc25ad3a79e4779492ac57
jaygr1 <jrib6@yahoo.com>
2015-12-01 20:20:22 -0500

    adding resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
   post 'logout' => "sessions#destroy"
-  get 'root' => "sessions#placeholder"
+
+  resources 'applications'
+  resources 'companys'
+  resources 'emails'
+  resources 'follow_ups'
+  resources 'interviews'
+  resources 'users'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
